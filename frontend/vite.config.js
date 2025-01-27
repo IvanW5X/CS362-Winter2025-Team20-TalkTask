6d27e58f01b5fb954f168871a20fee0c4e538ed2
IvanW5X <ivanwong169@gmail.com>
2025-01-27 11:40:53 -0800

    Configured frontend to use .env file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({path: path.resolve(__dirname, "../.env")});
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': process.env,
+  },
   plugins: [react()],
-})
+});
