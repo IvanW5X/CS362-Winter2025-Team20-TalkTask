72c875db0e23bd245954e17673163e7c13f59749
IvanW5X <ivanwong169@gmail.com>
2025-01-27 20:38:18 -0800

    Seperated connecting servers with function definitions for index.js and server.js. Moved entry point from server -> index

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
 /********************************************************************
  * File Name: index.js
  * Date: 1/27/2025
- * Description: JS file for connecting servers, databases, and APIs
+ * Description: JS file for connecting servers, databases, and APIs.
+ *              Entry point for 
  * Author(s): CS 362-Team 20
  ********************************************************************/
+
+import { startServer, connectDatabase } from "./server.js";
+
+connectDatabase();
+
+startServer();
--- backend/server.js
+++ backend/server.js
@@ -10,26 +10,32 @@ import dotenv from "dotenv";
 import path from "path";
 import cors from "cors";
 
+const app = express();
+
 // Add API key path
 const envFilePath = path.resolve("../", "./.env");
 dotenv.config({ path: envFilePath });
 
-const app = express();
-const SERVER_PORT = process.env.SERVER_PORT;
-const AI_ACCESS_TOKEN = process.env.AI_API_KEY;
-const FRONTEND_URL = process.env.FRONTEND_URL;
-const corsOptions = {
-  origin: FRONTEND_URL,
+export const connectDatabase = () => {
+  const AI_ACCESS_TOKEN = process.env.AI_API_KEY;
+  const FRONTEND_URL = process.env.FRONTEND_URL;
+  const corsOptions = {
+    origin: FRONTEND_URL,
+  };
+  // Enable cross origin resource sharing
+  app.use(cors(corsOptions));
+
+  // TESTING PURPOSES, INFO TO BE DISPLAYED
+  app.get("/testing", (req, res) => {
+    res.json({ info: ["data_1", "data_2", "data_3", "data_4"] });
+  });
 };
 
-// Enable cross origin resource sharing
-app.use(cors(corsOptions));
+// Listening function
+export const startServer = () => {
+  const SERVER_PORT = process.env.SERVER_PORT;
 
-// TESTING PURPOSES, INFO TO BE DISPLAYED
-app.get("/testing", (req, res) => {
-  res.json({ info: ["data_1", "data_2", "data_3", "data_4"] });
-});
-
-app.listen(SERVER_PORT, () => {
-  console.log(`Server is listening on port ${SERVER_PORT}`);
-});
+  app.listen(SERVER_PORT, () => {
+    console.log(`Server is listening on port ${SERVER_PORT}`);
+  });
+};
