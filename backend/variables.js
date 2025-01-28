929a7a5e672ca214c23157d70b3217b88359700e
IvanW5X <ivanwong169@gmail.com>
2025-01-27 16:33:58 -0800

    Added variables.js to backend, will be used for creating and managing common variables

---
+++ backend/variables.js
@@ -0,0 +1,6 @@
+/********************************************************************
+ * File Name: variables.js
+ * Date: 1/27/2025
+ * Description: JS file for storing and managing common variables
+ * Author(s): CS 362-Team 20
+ ********************************************************************/
