e5d0fa96f6bdcc5b4f9ac6dd6c4ce90d880f60e4
IvanW5X <ivanwong169@gmail.com>
2025-01-27 16:33:19 -0800

    Added index.js to backend, will be used for connecting database and APIs

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+/********************************************************************
+ * File Name: index.js
+ * Date: 1/27/2025
+ * Description: JS file for connecting servers, databases, and APIs
+ * Author(s): CS 362-Team 20
+ ********************************************************************/
