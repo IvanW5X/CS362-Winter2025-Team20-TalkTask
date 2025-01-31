d9c9004a6e2d687c8681b1c411662a6e7e0f1754
raymondcen <raymondcen07@gmail.com>
2025-01-30 23:24:56 -0800

    example implentation with button

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -5,19 +5,37 @@
  * Author(s): CS 362-Team 20
  ********************************************************************/
 
-import React from "react";
+import React, { useState } from "react";
 import styles from "./home.module.css";
 import { Navbar } from "../../components/navbar/navbar";
 import { Sidebar } from "../../components/sidebar/sidebar";
 import { Tasks } from "../../components/tasks/tasks";
 
+
+//testing button
+import { recognition } from "../../services/webSpeech";
+
+
 export const Home = () => {
+    const [isListening, setIsListening] = useState(false);
+  
+    const toggleMic = () => {
+      if (isListening) {
+        recognition.stop();
+        console.log("Microphone stopped");
+      } else {
+        recognition.start();
+        console.log("Microphone started");
+      }
+      setIsListening(!isListening);
+    };
+  
     return (
-        <div className={styles.container}>
-            <h1>Home Page</h1>
-            <Navbar />
-            <Sidebar />
-            <Tasks />
-        </div>
+      <div className={styles.container}>
+        <h1>Home Page</h1>
+        <button type="button" onClick={toggleMic}>
+          {isListening ? "Stop Voice Input" : "Start Voice Input"}
+        </button>
+      </div>
     );
-};
+  };
