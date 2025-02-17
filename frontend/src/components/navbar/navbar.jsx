c213cf820334a89da6c8cf118fe6893e9a06299b
nwood11 <167386270+nwood11@users.noreply.github.com>
2025-02-17 15:59:01 -0800

    Update navbar.jsx with expanding list
    Still need to add links to pages

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -5,28 +5,28 @@
  * Author(s): CS 362-Team 20
  ********************************************************************/
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styles from "./navbar.module.css";
 import { Link } from "react-router-dom"; // For navigation
 
 export const Navbar = () => {
-  const [apiLinks, setApiLinks] = useState([]);
+  const [showItems, setShowItems] = useState(false); // state to toggle visibility of items
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts") //change to correct api, just basic stuff rn
-      .then((response) => response.json())
-      .then((data) => setApiLinks(data.map((item) => ({ id: item.id, name: item.title })))) //store
-      .catch((error) => console.error("Error fetching navigation links:", error)); //error handling
-  }, []);
+  const toggleItems = () => {
+    setShowItems(!showItems); // Toggle visibility of items
+  };
 
- return (
-    <nav className={styles.menuIcon}>
-      <div className={styles.menuIcon}>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.menuIcon} onClick={toggleItems}></div>
+      
+      <ul className={styles.navbarList}>
+        <ul className={`${styles.subMenu} ${showItems ? styles.show : ""}`}>
+          <li className={styles.navbarItem}>page 1</li>
+          <li className={styles.navbarItem}>page 2</li>
+          <li className={styles.navbarItem}>page 3</li>
+        </ul>
+      </ul>
     </nav>
   );
 };
-
