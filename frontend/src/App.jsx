5655b49962c264bab124a6f6a7f8c0bb64808998
Raymond Cen <raymondcen07@gmail.com>
2025-02-19 16:20:23 -0800

    minor change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,9 +9,6 @@ import React from "react";
 import styles from "./App.module.css";
 import { Home } from "./pages/home/home";
 import logo from "../assets/TalkTaskLogo1.png";
-
-//test, delete later
-import { TestSpeech } from "./pages/WebSpeechTest/testspeech";
 import profilePicture from "../assets/GenericAvatar.png";
 
 // Implement funcitonality for displaying about page or user home page; based on if user logged in
@@ -20,20 +17,10 @@ import profilePicture from "../assets/GenericAvatar.png";
 function App() {
   return (
     <div className={styles.App}>
-      <div className={styles.curvedsquare}>
+      <div className={styles.topbar}>
         <img className={styles.logo} src={logo} alt="TalkTask Logo" />
         <img className={styles.profilePicture} src={profilePicture} alt="Profile Picture" />
       </div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-
-          {/* test speech, delete later http://localhost:5173/testspeech*/}
-          <Route path="/testspeech" element={<TestSpeech />} /> 
-
-        </Routes>
-      </BrowserRouter>
-      <h1>Hello TalkTask</h1>
       <main>
         <Home />
       </main>
