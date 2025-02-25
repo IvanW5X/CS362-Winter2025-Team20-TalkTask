4d1b9b59f24ef0ac1ffce147b5ed1b59a126d8cd
IvanW5X <ivanwong169@gmail.com>
2025-02-25 11:28:50 -0800

    Moved variables file

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
-import { FRONTEND_URL } from "./variables.js";
+import { FRONTEND_URL } from "./utils/variables.js";
 
 const app = express();
 const corsOptions = {
