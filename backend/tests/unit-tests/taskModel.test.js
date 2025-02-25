19cd10f4a31b60c1c88c4f07bb61a6284b787cc5
IvanW5X <ivanwong169@gmail.com>
2025-02-24 22:03:24 -0800

    updated backend test

--- backend/tests/unit-tests/taskModel.test.js
+++ backend/tests/unit-tests/taskModel.test.js
@@ -30,6 +30,7 @@ describe('Task Model - Unit Tests', () => {
   it('should create a new task successfully', async () => {
     const task = new Task({
       taskID: 1,
+      title: "test title",
       userId: new mongoose.Types.ObjectId(),
       description: 'Test task description',
       status: 'pending'
@@ -45,6 +46,7 @@ describe('Task Model - Unit Tests', () => {
   it('should mark a task as complete', async () => {
     const task = new Task({
       taskID: 2,
+      title: "test title 2",
       userId: new mongoose.Types.ObjectId(),
       description: 'Another test task',
       status: 'in-progress'
