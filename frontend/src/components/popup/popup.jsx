b9f70c2aca99a385996532d1c939381427824dba
Raymond Cen <raymondcen07@gmail.com>
2025-02-26 15:17:30 -0800

    minor changes

--- frontend/src/components/popup/popup.jsx
+++ frontend/src/components/popup/popup.jsx
@@ -6,15 +6,18 @@ export const PopUp = ()=>{
     return (
         <>
         <div className="z-[10001] absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
-                <form className="w-[1000px] h-[800px] bg-blue-500 rounded-3xl">
-                    <fieldset className="w-full h-fit flex justify-center">
-                        <legend className="bg-white rounded-2xl text-[30px] font-semibold mt-4 w-[600px] text-center p-4">Add Task</legend>
-                        
-                        <p>
-                            <label></label>
-                            <input></input>
-                        </p>
-                    </fieldset>  
+                <form className="flex flex-col w-[1000px] h-[800px] bg-blue-500 rounded-3xl items-center">
+                    <div className="bg-white mt-4 p-4 w-[600px] text-[40px] font-bold text-center rounded-2xl">Add Task</div>
+                    
+                    <p>
+                        <label className="bg-green-200">Helo</label>
+                        <input className={`border-[2px]`}
+                            type="text"
+                            placeholder="Name of the task (25 characters)"
+                            maxlength="25"
+                           
+                        />
+                    </p>
                 </form>
             </div>
         </>
