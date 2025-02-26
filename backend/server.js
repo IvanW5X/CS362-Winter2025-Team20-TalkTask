d195db229a407b6623d14089163eadc0988a8a59
Battle-Potato <tvosburg03@gmail.com>
2025-02-26 15:39:34 -0800

    Added back end support for Auth0

--- backend/server.js
+++ backend/server.js
@@ -9,18 +9,45 @@ import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
-import { FRONTEND_URL } from "./variables.js";
+import { FRONTEND_URL, AUTH0_AUDIENCE, AUTH0_DOMAIN} from "./variables.js";
+import helmet from "helmet";
+import { auth } from "express-oauth2-jwt-bearer";
+
 
 const app = express();
 const corsOptions = {
   origin: FRONTEND_URL,
 };
 
+if (
+  !AUTH0_DOMAIN ||
+  !AUTH0_AUDIENCE ||
+  AUTH0_AUDIENCE === "YOUR_API_IDENTIFIER"
+) {
+  console.log(
+    "Exiting: Please make sure that auth_config.json is in place and populated with valid domain and audience values"
+  );
+
+  process.exit();
+}
+
+app.use(helmet());
 // Enable cross origin resource sharing
 app.use(cors(corsOptions));
 
-// Use routes
-app.use("/users", userRoutes);
-app.use("/tasks", taskRoutes);
+const checkJwt = auth({
+  audience: AUTH0_AUDIENCE,
+  issuerBaseURL: AUTH0_DOMAIN,
+  algorithms: ["RS256"],
+});
+
+// Use private routes
+app.use("/users", checkJwt, userRoutes);
+app.use("/tasks", checkJwt, taskRoutes);
+
+//use public routes
+// app.use("/users", userRoutes);
+// app.use("/tasks", taskRoutes);
+
 
 export default app;
