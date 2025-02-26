e4f6c10a8f1c574b726e5eb899a328fedfcfad49
IvanW5X <ivanwong169@gmail.com>
2025-02-25 21:09:28 -0800

    Minor changes

--- backend/tests/integration-tests/taskAPI.test.js
+++ backend/tests/integration-tests/taskAPI.test.js
@@ -1,9 +1,9 @@
-import mongoose from 'mongoose';
-import request from 'supertest';
-import app from '../../server.js';
-import { Task } from '../../db/models/taskModel.js';
+import mongoose from "mongoose";
+import request from "supertest";
+import app from "../../server.js";
+import { Task } from "../../db/models/taskModel.js";
 
-describe('Task API - Integration Tests', () => {
+describe("Task API - Integration Tests", () => {
   beforeAll(async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
@@ -17,20 +17,20 @@ describe('Task API - Integration Tests', () => {
 
   let taskId;
 
-  it('should create a new task via API', async () => {
-    const res = await request(app).post('/api/tasks').send({
+  it("should create a new task via API", async () => {
+    const res = await request(app).post("/api/tasks").send({
       taskID: 5,
       userId: new mongoose.Types.ObjectId(),
-      description: 'Test API Task',
-      status: 'pending'
+      description: "Test API Task",
+      status: "pending",
     });
 
     expect(res.status).toBe(201);
-    expect(res.body.description).toBe('Test API Task');
+    expect(res.body.description).toBe("Test API Task");
     taskId = res.body._id;
   });
 
-  it('should delete a task via API', async () => {
+  it("should delete a task via API", async () => {
     const res = await request(app).delete(`/api/tasks/${taskId}`);
     expect(res.status).toBe(200);
 
---
+++ frontend/src/utils/variables.js
@@ -0,0 +1,8 @@
+/********************************************************************
+ * File Name: variables.js
+ * Date: 2/25/2025
+ * Description: JS file for storing common variables
+ * Author(s): CS 362-Team 20
+ ********************************************************************/
+
+export const VITE_BACKEND_URL = process.env.VITE_BACKEND_URL;
