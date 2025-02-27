a676cb8578241cb2e06fe1b89b5f37c88fb32be9
Raymond Cen <raymondcen07@gmail.com>
2025-02-27 12:52:37 -0800

    task management

--- frontend/src/components/taskma/taskma.jsx
+++ frontend/src/components/taskma/taskma.jsx
@@ -8,16 +8,53 @@ import { IoStar } from "react-icons/io5";
 import { AddPopUp } from "../addpopup/addpopup";
 
 export const TasksManagement = () =>{
+
+  const[addMenuV, setAddMenuV] = useState(false);
+
     return(
-     <div className="bg-red-500 w-[600px] ml-[30px] rounded-[10px]">
+     <div className="bg-[#E5E5E5] w-[350px] ml-[30px] rounded-[10px]">
+        {/* add menu */}
+        {addMenuV && <AddPopUp onClose={() => setAddMenuV(false)} />}
+
           {/* Title */}
-          <div className="flex text-center m-5 text-[20px] font-semibold bg-white px-5 py-3 rounded-[10px] shadow">
+          <div className="flex text-center m-5 text-[20px] font-semibold bg-white px-5 py-3 rounded-[10px]">
             <h2 className="w-full text-center">Task Managment</h2>
           </div>
+
+
           {/* actions */}
-          <div className="mx-5 mb-5 space-y-4">
-            
+          <div className="mx-7 mb-5 space-y-12 text-[20px] font-medium relative ">
+
+            {/* add task */}
+            <div className={`flex mt-12 cursor-pointer h-[40px] bg-white rounded-2xl justify-center items-center shadow`}
+                  onClick={()=>setAddMenuV(!addMenuV)}>
+              Add Task 
+              <CiCirclePlus className="absolute right-3 text-[25px]"/>
+            </div>
+
+            {/* clear completed tasks */}
+            <div className={`flex cursor-pointer h-[40px] bg-white rounded-2xl justify-center items-center shadow`}>
+              Clear Completed Tasks
+              <FaCheck className="absolute right-3 "/>
+            </div>
+
+            {/* voice commands */}
+            <div className={`flex cursor-pointer h-[40px] bg-white rounded-2xl justify-center items-center shadow`}>
+            Voice Commands
+              <MdOutlineIntegrationInstructions className="absolute right-3"/>
+            </div>
+
+            <div className={`flex cursor-pointer h-[40px] bg-white rounded-2xl justify-center items-center shadow`}>
+              Suggest a Task
+              <IoStar className="absolute right-3"/>
+            </div>
+
+            {/* mic button */}
+            <div className={`flex cursor-pointer h-[40px] bg-[#37E03A] rounded-2xl justify-center items-center shadow`}>
+              <FaMicrophone className="text-[30px] text-white" />
+            </div>
+
           </div>
-        </div>
+      </div>
     )
 };
