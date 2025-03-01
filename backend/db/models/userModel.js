26a38efda0df07192996bb502427294e31cc8996
IvanW5X <ivanwong169@gmail.com>
2025-03-01 13:00:38 -0800

    Removed unecessary attributes

--- backend/db/models/userModel.js
+++ backend/db/models/userModel.js
@@ -8,14 +8,11 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    CustomerID: { type: Number, unique: true, required: true },
+    userID: { type: Number, unique: true, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }, // Should be hashed before saving
-    dob: { type: Date },
+    password: { type: String, required: true },             // Should be hashed before saving
     Fname: { type: String, required: true },
-    Mname: { type: String },
     Lname: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
 });
 
 export const User = mongoose.model('User', userSchema);
