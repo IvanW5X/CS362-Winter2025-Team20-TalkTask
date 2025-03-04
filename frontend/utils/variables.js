9daa6d9f319d9fb05c325467ec8907e8cd60e4a9
IvanW5X <ivanwong169@gmail.com>
2025-03-03 17:31:08 -0800

    Replaced REACT prefix on env vars to VITE for Vite builds

--- frontend/utils/variables.js
+++ frontend/utils/variables.js
@@ -6,6 +6,6 @@
  ********************************************************************/
 
 export const VITE_BACKEND_URL = process.env.VITE_BACKEND_URL;
-export const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN;
-export const AUTH0_AUDIENCE = process.env.REACT_APP_AUTH0_AUDIENCE;
-export const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID;
+export const AUTH0_DOMAIN = process.env.VITE_APP_AUTH0_DOMAIN;
+export const AUTH0_AUDIENCE = process.env.VITE_APP_AUTH0_AUDIENCE;
+export const AUTH0_CLIENT_ID = process.env.VITE_APP_AUTH0_CLIENT_ID;
