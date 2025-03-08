19dbefcf1eb379d2ea761e99d83f231d0696ccc0
IvanW5X <ivanwong169@gmail.com>
2025-03-08 15:18:45 -0800

    Edit task now automatically taskes the selected category

--- frontend/src/components/task-management/addpopup/addpopup.jsx
+++ frontend/src/components/task-management/addpopup/addpopup.jsx
@@ -108,7 +108,7 @@ export const AddPopUp = ({ onClose, selectedCategory }) => {
     <>
       <div className="z-[10001] fixed top-0 left-0 w-full h-full bg-black/40 flex items-center justify-center ">
         <form
-          className="relative border-3 flex flex-col w-[900px] h-[620px] bg-gray-200 rounded-3xl items-center overflow-x-auto"
+          className="relative border-3 flex flex-col w-[650px] h-[620px] bg-gray-200 rounded-3xl items-center overflow-x-auto"
           onSubmit={handleSubmit}
         >
           {/* Close Button */}
